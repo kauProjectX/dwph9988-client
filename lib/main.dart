3a63df80fb8ff2b9d0f2a10e30edd032ea1b318a
Youngmin Koo <youngminz.kr@gmail.com>
2024-10-01 17:35:10 +0900

    feat: 기본 폰트 변경

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
+        fontFamily: 'Gmarket Sans',
       ),
       home: const LoginScreen(),
     );
