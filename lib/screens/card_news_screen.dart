9538f9d7d2fc59b4ffe43ab9c5b1978555899e81
Youngmin Koo <youngminz.kr@gmail.com>
2024-11-15 21:13:14 +0900

    fix: unused import

--- lib/screens/card_news_screen.dart
+++ lib/screens/card_news_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:lucide_icons_flutter/lucide_icons.dart';
 
 class CardNewsScreen extends StatelessWidget {
   const CardNewsScreen({super.key});
