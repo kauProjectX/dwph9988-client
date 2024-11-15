a3ecfe64335d706cf300096ec905d389f213abe6
Youngmin Koo <youngminz.kr@gmail.com>
2024-11-15 15:43:44 +0900

    feat: 로그인 페이지 개편

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
-import 'sign_up_screen.dart';
-import 'app_screen.dart';
-import 'find_idpw_screen.dart';
+
 import '../controllers/user_controller.dart';
+import 'app_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -16,70 +15,9 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildInputFields() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 27),
-      child: SizedBox(
-        width: 270,
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                labelText: '아이디',
-                labelStyle: const TextStyle(
-                  color: Colors.grey,
-                  fontSize: 20,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(color: Colors.grey),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(color: Colors.grey),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(color: Colors.grey),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                labelText: '비밀번호',
-                labelStyle: const TextStyle(
-                  color: Colors.grey,
-                  fontSize: 20,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(color: Colors.grey),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(color: Colors.grey),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(color: Colors.grey),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   Widget _buildLoginButton(BuildContext context) {
-    return SizedBox(
-      width: 200,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
       child: ElevatedButton(
         onPressed: () {
           Get.dialog(
@@ -117,7 +55,7 @@ class LoginScreen extends StatelessWidget {
           ),
         ),
         child: const Text(
-          '로그인',
+          '카카오 로그인',
           style: TextStyle(
             fontSize: 22,
             fontWeight: FontWeight.w600,
@@ -128,67 +66,34 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildSignUpButton() {
-    return TextButton(
-      onPressed: () {
-        Get.to(() => const SignUpScreen());
-      },
-      child: const Text(
-        '회원가입',
-        style: TextStyle(fontSize: 18),
-      ),
-    );
-  }
-
-  Widget _buildFindCredentialsButton() {
-    return TextButton(
-      onPressed: () {
-        Get.to(() => const FindIDPWScreen());
-      },
-      child: const Text(
-        '아이디/비밀번호 찾기',
-        style: TextStyle(fontSize: 18),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFFFF8F7),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(height: 150),
-                const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 25),
-                    child: Text(
-                      '고령자 온열 질환 예방 서비스',
-                      style: TextStyle(
-                        fontSize: 15.5,
-                        fontWeight: FontWeight.w500,
-                        color: Color(0xFFFF7171),
-                      ),
-                    ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: EdgeInsets.only(left: 25),
+                child: Text(
+                  '고령자 온열 질환 예방 서비스',
+                  style: TextStyle(
+                    fontSize: 15.5,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFFFF7171),
                   ),
                 ),
-                const SizedBox(height: 0),
-                _buildLogo(), // 로고를 텍스트 아래에 배치
-                const SizedBox(height: 50),
-                _buildInputFields(),
-                const SizedBox(height: 50),
-                _buildLoginButton(context),
-                const SizedBox(height: 20),
-                _buildSignUpButton(),
-                const SizedBox(height: 0),
-                _buildFindCredentialsButton(),
-              ],
+              ),
             ),
-          ),
+            _buildLogo(),
+            const SizedBox(height: 50),
+            _buildLoginButton(context),
+            const SizedBox(height: 100),
+          ],
         ),
       ),
     );
